beb6d364c4a0cbee055a88082f40dbb1af66d78e
kunlexyz <bobbykay007@gmail.com>
2022-12-08 20:24:32 +0100

    biology_installed.html

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v3';
-const dynamicCacheName = 'site-dynamic-v3';
+const staticCacheName = 'site-static-v5';
+const dynamicCacheName = 'site-dynamic-v5';
 const assets = [
   '/',
   '/biology.html',
